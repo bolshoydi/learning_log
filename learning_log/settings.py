b7a67bf73135b22dea29f85605716eac80427cdb
Dmitry Litvinov <bolshoydi@gmail.com>
2017-12-23 19:51:54 +0300

    rfactor some names

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'learnin_log.urls'
+ROOT_URLCONF = 'learning_log.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'learnin_log.wsgi.application'
+WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
